598d9ca7c0f2d252f3ced016ed90f28dfa416e90
MannexDera <emmanuelchi545@gmail.com>
2025-01-21 16:09:14 +0100

    new update

---
+++ src/js/controllers/App.js
@@ -0,0 +1,7 @@
+import { navLinkClicks } from '../utils/header.links.js';
+
+
+const init = () => {
+    navLinkClicks();
+};
+init();
---
+++ src/js/utils/header.links.js
@@ -0,0 +1,10 @@
+export const navLinkClicks = () => {
+    const navLinks = document.querySelectorAll('.nav__link');
+
+    navLinks.forEach(navItem => {
+        navItem.addEventListener('click', () => {
+            navLinks.forEach(item => item.classList.remove('active'));
+            navItem.classList.add('active');
+        });
+    });
+};
